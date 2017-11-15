ecb1fdcc212531dd5b1649c428a7a73ca41096fe
Erik Tammpere <etammpere@gmail.com>
2017-11-15 06:39:23 +0000

    Big commit: formatted navbar, added and styled a new-tweet form as well as a tweet feed and tweet article templates. Set up asynchronouse AJAX form submission as well as asyncronous AJAX tweet rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,63 @@
+
+function createTweetElement(data) {
+  let name = data.user.name;
+  let handle = data.user.handle;
+  let content = data.content.text;
+  let timeStamp = new Date(data.created_at);
+  let avatar = data.user.avatars.small;
+
+
+  var $tweet = $('section.tweet-feed').append(`
+    <article class="tweet">
+          <header>
+            <img src="${avatar}">
+            <h2>${name}</h2>
+            <p>${handle}</p>
+          </header>
+          <div class="body">
+            <p>${content}</p>
+          </div>
+          <footer>
+            <p>${timeStamp}</p>
+            <div>
+              <i class="fa fa-flag" aria-hidden="true"></i>
+              <i class="fa fa-retweet" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+            </div>
+          </footer>
+        </article>`);
+
+  return $tweet;
+}
+
+function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        renderTweets(data);
+      }
+    });
+  }
+
+function renderTweets(data) {
+  data.forEach(function(tweet) {
+    createTweetElement(tweet);
+  });
+}
+
+$(document).ready(function() {
+
+  loadTweets();
+
+});
+
+
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".new-tweet").on('keyup', 'input[type=text], textarea', function () {
+    var $charsLeft = 140 - this.value.length;
+    var $counter = $(this).siblings('.counter');
+    $charsLeft < 0 ? $counter.addClass('over-length-limit') : $counter.removeClass('over-length-limit');
+    $counter.text($charsLeft);
+  });
+});
