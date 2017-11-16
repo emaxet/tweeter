01d51deed5442fb51bd18a119b1e9013dcb157bf
Erik Tammpere <etammpere@gmail.com>
2017-11-16 08:28:42 +0000

    Refactoring

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -66,32 +66,30 @@ $(document).ready(function() {
   loadTweets();
 
   $(function() {
-          const $form = $('.new-tweet');
-          $form.on('submit', function(event) {
-            event.preventDefault();
-            const $tweetLength = 140 - +$('.counter').text();
-            if ($tweetLength <= 140 && $tweetLength > 0) {
-              const $data = $('.new-tweet :input').serialize();
-              $.ajax({
-                url: '/tweets',
-                method: 'POST',
-                data: $data,
-                success: function(data) {
-                  $('article').remove();
-                  loadTweets();
-                  resetNewTweetForm();
-                }
-              });
-            } else if ($tweetLength > 140){
-              $('#plus-140').css({display: 'block' });
-              $('#empty-tweet').css({display: 'none'});
-
-            } else {
-              $('#empty-tweet').css({display: 'block'});
-              $('#plus-140').css({display: 'none' });
-            }
-          });
+    const $form = $('.new-tweet');
+    $form.on('submit', function(event) {
+      event.preventDefault();
+      const $tweetLength = 140 - +$('.counter').text();
+      if ($tweetLength <= 140 && $tweetLength > 0) {
+        const $data = $('.new-tweet :input').serialize();
+        $.ajax({
+          url: '/tweets',
+          method: 'POST',
+          data: $data,
+          success: function(data) {
+            $('.tweet-feed article').remove();
+            loadTweets();
+            resetNewTweetForm();
+          }
         });
-
+      } else if ($tweetLength > 140){
+        $('#plus-140').css({display: 'block' });
+        $('#empty-tweet').css({display: 'none'});
+      } else {
+        $('#empty-tweet').css({display: 'block'});
+        $('#plus-140').css({display: 'none' });
+      }
+    });
+  });
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".new-tweet").on('keyup', 'input[type=text], textarea', function () {
+  $(".new-tweet").on('input', 'input[type=text], textarea', function () {
     var $charsLeft = 140 - this.value.length;
     var $counter = $(this).siblings('.counter');
     $charsLeft < 0 ? $counter.addClass('over-length-limit') : $counter.removeClass('over-length-limit');
