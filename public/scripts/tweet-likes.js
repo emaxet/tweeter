6a55a2f666d394be0a212e500e1f425d858fc5e2
Erik Tammpere <etammpere@gmail.com>
2017-11-17 04:57:08 +0000

    likes are now updated on the tweet article live when they fa-heart icon is pressed

--- public/scripts/tweet-likes.js
+++ public/scripts/tweet-likes.js
@@ -1,13 +1,20 @@
 
+function liveUpdateLikeCount(data) {
+  const dataObject = JSON.parse(data);
+  return dataObject;
+}
+
 function tweetLike() {
   $('.fa-heart').on('click', (e) => {
     $(e.target).toggleClass('likedTweet');
+    const $likes = $(e.target).siblings('p');
     const $id = $(e.target).siblings('.tweetId').text();
     $.ajax({
       url: `/tweets/${$id}/like`,
       method: 'POST',
       success: (data) => {
-        alert(data);
+        const dataObject = liveUpdateLikeCount(data);
+        $likes.text(`${dataObject.likes}`);
       }
     })
   });
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -50,7 +50,10 @@ module.exports = function(DataHelpers) {
     const tweet = db.collection('tweets').findOne({ _id: new ObjectId(tweetID) });
     tweet.then(function(tweet) {
        db.collection('tweets').updateOne({ _id: new ObjectId(tweetID)}, { $set: { 'likes': tweet.likes + 1 } }) ;
+       res.status(200);
+       res.send(JSON.stringify(tweet));
     });
+
   });
 
   return tweetsRoutes;
